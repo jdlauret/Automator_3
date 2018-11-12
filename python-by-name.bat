3a8378e08f20a69dec2f69afadb66e3f0927e3ad
Unknown <jlauret89@gmail.com>
2018-11-12 15:17:51 -0700

    Startup File Change
    Updated Startup file to run out of new location

--- python-by-name.bat
+++ python-by-name.bat
@@ -1,6 +1,6 @@
 set PYTHON_HOME=%~dp0\venv\Scripts
 set PYTHON_NAME=%1.exe
-set AUTOMATOR_HOME=%C:\Users\jonathan.lauret\Google Drive\Projects\Python Projects\Current Projects\Automator_3
+set AUTOMATOR_HOME=%C:\Automator_3_v1.0
 copy "%PYTHON_HOME%\python.exe" "%AUTOMATOR_HOME%\%PYTHON_NAME%"
 set args=%*
 set args=%args:* =%
