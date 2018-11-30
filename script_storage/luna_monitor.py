c10f6c766f95cfbf68c5e5b98ae1e2d952ceb611
JD <jlauret89@gmail.com>
2018-11-29 17:16:28 -0700

    FIX
    Corrected luna_dev to search for dev instead of Production

--- script_storage/luna_monitor.py
+++ script_storage/luna_monitor.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     restart_luna_bat = 'C:\\Luna_Production\\restart_server.bat'
     restart_luna_dev_bat = 'C:\\Luna_Dev\\dev_server_restart.bat'
     luna_production = UrlMonitor('Luna_Production_Server', 'http://luna.vivintsolar.com', restart_luna_bat)
-    luna_dev = UrlMonitor('Luna_Production_Server', 'http://luna.vivintsolar.com:8000', restart_luna_dev_bat)
+    luna_dev = UrlMonitor('Luna_Dev_Server', 'http://luna.vivintsolar.com:8000', restart_luna_dev_bat)
     luna_production.run()
     luna_dev.run()
